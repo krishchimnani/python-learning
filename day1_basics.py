e12b7cb3d1b5f59da2d7c62f053ef454b55edbff
krishchimnani <chimnanikrish7@gmail.com>
2025-08-15 03:55:08 +0530

    Add files via upload

---
+++ day1_basics.py
@@ -0,0 +1,55 @@
+print("hello world")
+a,b=10,20
+print (a/b)
+
+
+
+
+import math 
+print(math.sqrt(4))
+
+
+
+
+
+
+from random import*
+print(randint(0,9))
+
+
+
+
+
+from random import*
+print(randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9),sep="")
+
+
+
+
+
+
+
+
+
+
+
+from random import*
+for i in range (10):
+    print(randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9),sep="")
+
+
+
+
+x=None
+print(type(x))
+
+
+
+
+from random import*
+for i in range (10):
+  print(randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9),sep="")
+
+
+
+
