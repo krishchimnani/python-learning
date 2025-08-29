948bec66a16393587c6327b5170a892933a3ab5f
krishchimnani <chimnanikrish7@gmail.com>
2025-08-29 21:13:43 +0530

    Add files via upload

---
+++ day3_basics.py
@@ -0,0 +1,102 @@
+print("     /|")
+print("    / | ")
+print("   /  | ")
+print("  /   |")
+print(" /    |")
+print("/_____|")
+
+
+print("\n")
+
+
+
+
+
+character_name="john"
+character_age= " 56.12 "
+character_gender="i am a gay" 
+character_real_gender="i am not gay i am straight"
+print("my name is " + character_name +"")
+print("i am" + character_age +"years old")
+print(""+character_gender+"")
+print(""+character_real_gender+"")
+print("i was just kidding")
+
+
+character_real_name= "gay paul"
+print("my rael name is" + character_real_name + "")
+
+
+
+
+
+
+print("\n")
+print("\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("just \n kidding")
+
+
+print("my name is\"just\" kidding")
+
+
+print("my name is (just)kidding") 
+
+
+
+
+
+
+
+
+
+name="kevin"
+print(name+" is not gay he is straight. ")
+
+
+
+
+
+phrase="just kidding"
+print(phrase .upper())
+
+print(phrase.lower())
+print(phrase.isupper())
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[6])
+print(phrase[11])
+print(phrase.index("t"))
+print(phrase.index("k"))
+print(phrase.replace("just","just not"))
+print(phrase.replace("kidding","kidding not"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
